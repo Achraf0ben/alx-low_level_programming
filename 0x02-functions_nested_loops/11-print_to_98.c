eac361774c0e2116c3aac3737841817b9933728a
Achraf Benhaddou <achraf.b.haddou@gmail.com>
2023-07-18 16:39:52 -0700

    t11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -7,36 +8,37 @@
  */
 
 void print_to_98(int n)
-	{
-		if (n <= 98)
-			{
-				for (; n <= 98; n++)
-				{
-					if (n == 98)
-					{
-						printf(“%d”, n);
-						printf(“\n”);
-						break;
-						}
-					else
-						{
-							printf(“%d, ”, n);
-							}
-				}
-			}
-		else
-		{
-			for (; n >= 98; n--)
-			{
-				if (n == 98)
-				{
-					printf(“%d”, n);
-					printf(“\n”);
-					break;
-				}
-				else
-				{
-					printf(“%d, ”, n);
-			}
-		}
-	}
+        {
+                if (n <= 98)
+                        {
+                                for (; n <= 98; n++)
+                                {
+                                        if (n == 98)
+                                        {
+                                                printf(“%d”, n);
+                                                printf(“\n”);
+                                                break;
+                                                }
+                                        else
+                                                {
+                                                        printf(“%d, ”, n);
+                                                        }
+                                }
+                        }
+                else
+                {
+                        for (; n >= 98; n--)
+                        {
+                                if (n == 98)
+                                {
+                                        printf(“%d”, n);
+                                        printf(“\n”);
+                                        break;
+                                }
+else
+                                {
+                                        printf(“%d, ”, n);
+                        }
+                }
+        }
+}
