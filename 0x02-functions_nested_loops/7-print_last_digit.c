34c409fdaa01a867b590968085d6380f6d27a9a1
Achraf Benhaddou <achraf.b.haddou@gmail.com>
2023-07-18 16:50:42 -0700

    final one today

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,19 +1,23 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_to_98 - count the natural numbers from n to 98.
- * @n: value to count from.
+ * print_last_digit - will print the last digit of a given number.
+ * @n: Number used to find the last digit.
  *
+ * Description: will print the last digit of a given number. If the
+ * number is a negative the value will be return unsigned int.
+ *
+ * Return: last digit as int value.
  */
 
 int print_last_digit(int n)
 {
-	int last;
-        last = n % 10;
-        if (last < 0)
-        {
-                last = last * -1;
-        }
-        _putchar(last + 'a');
-        return (last);
+	n %= 10;
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	_putchar('0' + n);
+	return (n);
 }
