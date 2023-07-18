a7efb3f13a5c9206c6dfc0fec4df12792026269d
Achraf Benhaddou <achraf.b.haddou@gmail.com>
2023-07-18 16:46:17 -0700

    final mand

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_to_98 - count the natural numbers from n to 98.
@@ -8,37 +8,31 @@
  */
 
 void print_to_98(int n)
-        {
-                if (n <= 98)
-                        {
-                                for (; n <= 98; n++)
-                                {
-                                        if (n == 98)
-                                        {
-                                                printf(“%d”, n);
-                                                printf(“\n”);
-                                                break;
-                                                }
-                                        else
-                                                {
-                                                        printf(“%d, ”, n);
-                                                        }
-                                }
-                        }
-                else
-                {
-                        for (; n >= 98; n--)
-                        {
-                                if (n == 98)
-                                {
-                                        printf(“%d”, n);
-                                        printf(“\n”);
-                                        break;
-                                }
-else
-                                {
-                                        printf(“%d, ”, n);
-                        }
-                }
-        }
+{
+	int i;
+
+	if (n == 98)
+	{
+		printf("98");
+	}
+	else if (n < 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+				printf(", ");
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+				printf(", ");
+
+		}
+	}
+	printf("\n");
 }
